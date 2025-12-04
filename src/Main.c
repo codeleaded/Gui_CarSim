829127a1bf1ff195b9af3be7b2ac223bd5e1ca90
codeleaded <codeleaded@gmail.com>
2025-12-04 18:00:38 +0100

    init commit

---
+++ src/Main.c
@@ -0,0 +1,68 @@
+#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/GSprite.h"
+#include "/home/codeleaded/System/Static/Library/NeuralNetwork.h"
+#include "/home/codeleaded/System/Static/Library/AffineTransform.h"
+
+
+float x;
+float y;
+float s;
+float a;
+
+AffineTransform transform;
+Sprite sp;
+
+void Setup(AlxWindow* w){
+    ResizeAlxFont(8,8);
+
+    transform = AffineTransform_New();
+    sp = Sprite_Load("./data/Car_Green_Fast.png");
+
+    x = 0.0f;
+    y = 0.0f;
+    s = 1.0f;
+    a = 0.0f;
+}
+void Update(AlxWindow* w){
+    if(Stroke(ALX_KEY_W).DOWN)      y -= 10.0f * w->ElapsedTime;
+    if(Stroke(ALX_KEY_S).DOWN)      y += 10.0f * w->ElapsedTime;
+    if(Stroke(ALX_KEY_A).DOWN)      x -= 10.0f * w->ElapsedTime;
+    if(Stroke(ALX_KEY_D).DOWN)      x += 10.0f * w->ElapsedTime;
+    
+    if(Stroke(ALX_KEY_UP).DOWN)     s -= 1.0f * w->ElapsedTime;
+    if(Stroke(ALX_KEY_DOWN).DOWN)   s += 1.0f * w->ElapsedTime;
+    
+    if(Stroke(ALX_KEY_LEFT).DOWN)   a -= 1.0f * w->ElapsedTime;
+    if(Stroke(ALX_KEY_RIGHT).DOWN)  a += 1.0f * w->ElapsedTime;
+
+    Clear(DARK_BLUE);
+
+    AffineTransform_Identity(&transform);
+    //AffineTransform_Trans(&transform,(Vec2){ x,y });
+    AffineTransform_Trans(&transform,(Vec2){ -(float)sp.w * 0.5f,-(float)sp.h * 0.5f });
+    //AffineTransform_Trans(&transform,(Vec2){ -x,-y });
+    AffineTransform_RotateZ(&transform,a);
+    AffineTransform_Trans(&transform,(Vec2){ (float)sp.w * 0.5f,(float)sp.h * 0.5f });
+    AffineTransform_Scale(&transform,(Vec2){ s,s });
+    
+    Rect area = AffineTransform_Rect(&transform,(Rect){ 0.0f,0.0f,sp.w,sp.h });
+    AffineTransform_Inverse(&transform);
+
+    for(int i = 0;i<area.d.y;i++){
+        for(int j = 0;j<area.d.x;j++){
+            const Vec2 rp = { area.p.x + j,area.p.y + i };
+            const Vec2 p = AffineTransform_Calc(&transform,rp);
+            const Pixel col = Sprite_Get(&sp,p.x,p.y);
+            RenderPixel(x + rp.x,y + rp.y,col);
+        }
+    }
+}
+void Delete(AlxWindow* w){
+    Sprite_Free(&sp);
+}
+
+int main(){
+    if(Create("Car Simulation",1900,1000,1,1,Setup,Update,Delete))
+        Start();
+    return 0;
+}
