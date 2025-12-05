ff02dd982530e6e5690cc7b3210bf164cfeb01c6
C-Reaper <gnu.c.reaper@gmail.com>
2025-12-05 02:31:07 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,39 +1,45 @@
 #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
-#include "/home/codeleaded/System/Static/Library/TDCar.h"
+#include "/home/codeleaded/System/Static/Library/TDWorld.h"
 
 
-TDCar car;
+TDWorld world;
 
 void Setup(AlxWindow* w){
     ResizeAlxFont(10,10);
 
-    car = TDCar_New((Vec2){ 0.0f,0.0f },"./data/Car_Green_Fast.png");
+    world = TDWorld_New(100,100,(Sprite[]){
+        Sprite_Load("./data/Track_Straight.png"),
+        Sprite_Load("./data/Track_Curve.png"),
+        Sprite_Load("./data/Track_Grass.png"),
+        Sprite_None()
+    });
+    TDWorld_LoadCar(&world,50.0f,50.0f,"./data/Car_Green_Fast.png");
 }
 void Update(AlxWindow* w){
-    if(Stroke(ALX_KEY_W).DOWN)          TDCar_Acc(&car, 1.0f,w->ElapsedTime);
-    if(Stroke(ALX_KEY_S).DOWN)          TDCar_Acc(&car,-1.0f,w->ElapsedTime);
-    if(Stroke(ALX_KEY_SPACE).DOWN)      TDCar_Break(&car,0.98f);
-    if(Stroke(ALX_KEY_ENTER).DOWN)      TDCar_Start(&car);
-    
-    //if(Stroke(ALX_KEY_UP).DOWN)       TDCar_Scale(&car, 1.0f * w->ElapsedTime);
-    //if(Stroke(ALX_KEY_DOWN).DOWN)     TDCar_Scale(&car,-1.0f * w->ElapsedTime);
+    if(Stroke(ALX_KEY_W).DOWN)          TDCar_Acc(&world.car, 1.0f,w->ElapsedTime);
+    if(Stroke(ALX_KEY_S).DOWN)          TDCar_Acc(&world.car,-1.0f,w->ElapsedTime);
+    if(Stroke(ALX_KEY_SPACE).DOWN)      TDCar_Break(&world.car,0.98f);
+    if(Stroke(ALX_KEY_ENTER).DOWN)      TDCar_Start(&world.car);
 
-    if(Stroke(ALX_KEY_UP).PRESSED)      TDCar_Gear(&car,car.gear + 1);
-    if(Stroke(ALX_KEY_DOWN).PRESSED)    TDCar_Gear(&car,car.gear - 1);
+    if(Stroke(ALX_KEY_UP).PRESSED)      TDCar_Gear(&world.car,world.car.gear + 1);
+    if(Stroke(ALX_KEY_DOWN).PRESSED)    TDCar_Gear(&world.car,world.car.gear - 1);
     
-    if(Stroke(ALX_KEY_LEFT).DOWN)       TDCar_Turn(&car, F32_PI * w->ElapsedTime);
-    if(Stroke(ALX_KEY_RIGHT).DOWN)      TDCar_Turn(&car,-F32_PI * w->ElapsedTime);
+    if(Stroke(ALX_KEY_LEFT).DOWN)       TDCar_Turn(&world.car, F32_PI * w->ElapsedTime);
+    if(Stroke(ALX_KEY_RIGHT).DOWN)      TDCar_Turn(&world.car,-F32_PI * w->ElapsedTime);
     
-    TDCar_Update(&car,w->ElapsedTime);
+    TDCar_Update(&world.car,w->ElapsedTime);
     
     Clear(DARK_BLUE);
 
-    TDCar_Render(WINDOW_STD_ARGS,&car);
-    TDCar_RenderSpeed(WINDOW_STD_ARGS,&window.AlxFont,&car,200.0f,GetHeight() - 200.0f,200.0f);
-    TDCar_RenderWTN(WINDOW_STD_ARGS,&window.AlxFont,&car,600.0f,GetHeight() - 200.0f,200.0f);
+    //TDCar_Render(WINDOW_STD_ARGS,&car);
+
+    TDWorld_Render(WINDOW_STD_ARGS,&world);
+
+    TDCar_RenderSpeed(WINDOW_STD_ARGS,&window.AlxFont,&world.car,200.0f,GetHeight() - 200.0f,200.0f);
+    TDCar_RenderWTN(WINDOW_STD_ARGS,&window.AlxFont,&world.car,600.0f,GetHeight() - 200.0f,200.0f);
 }
 void Delete(AlxWindow* w){
-    TDCar_Free(&car);
+    TDWorld_Free(&world);
 }
 
 int main(){
